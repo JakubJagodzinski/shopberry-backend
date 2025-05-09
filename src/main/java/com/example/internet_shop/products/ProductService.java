62ff55797bcceca0379f3687b65dfdaab40ffff0
JagodaJakub <kubajagjag@gmail.com>
2025-05-10 00:51:39 +0200

    Added product name validation (empty, null)

--- src/main/java/com/example/internet_shop/products/ProductService.java
+++ src/main/java/com/example/internet_shop/products/ProductService.java
@@ -16,6 +16,8 @@ public class ProductService {
 
     private final String PRODUCT_NOT_FOUND_MESSAGE = "Product not found";
     private final String PRODUCT_WITH_THAT_NAME_ALREADY_EXISTS_MESSAGE = "Product with that name already exists";
+    private final String PRODUCT_NAME_CANNOT_BE_EMPTY_MESSAGE = "Product name cannot be empty";
+    private final String PRODUCT_NAME_CANNOT_BE_NULL_MESSAGE = "Product name cannot be null";
     private final String PRODUCT_PRICE_MUST_BE_GREATER_THAN_ZERO_MESSAGE = "Product price must be greater than zero";
     private final String PRODUCT_DISCOUNT_PERCENT_VALUE_CAN_T_BE_NEGATIVE_MESSAGE = "Product discount percent value can't be negative";
     private final String PRODUCT_DISCOUNT_PERCENT_VALUE_CAN_T_BE_GREATER_THAN_100_MESSAGE = "Product discount percent value can't be greater than 100";
@@ -43,6 +45,14 @@ public class ProductService {
     public ProductDto createProduct(CreateProductDto createProductDto) throws IllegalArgumentException {
         if (productRepository.existsByProductName(createProductDto.getProductName())) {
             throw new IllegalArgumentException(PRODUCT_WITH_THAT_NAME_ALREADY_EXISTS_MESSAGE);
+        }
+
+        if (createProductDto.getProductName() == null) {
+            throw new IllegalArgumentException(PRODUCT_NAME_CANNOT_BE_NULL_MESSAGE);
+        }
+
+        if (createProductDto.getProductName().isEmpty()) {
+            throw new IllegalArgumentException(PRODUCT_NAME_CANNOT_BE_EMPTY_MESSAGE);
         }
 
         Product product = new Product();
