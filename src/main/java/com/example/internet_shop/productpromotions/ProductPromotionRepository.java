83cf56904430a95a099ace6a1777afd254d8bf54
JagodaJakub <kubajagjag@gmail.com>
2025-05-12 00:35:35 +0200

    Added CRUD controller and all required classes including service, dtoMapper and relevant dtos for ProductPromotion entity

--- src/main/java/com/example/internet_shop/productpromotions/ProductPromotionRepository.java
+++ src/main/java/com/example/internet_shop/productpromotions/ProductPromotionRepository.java
@@ -3,6 +3,17 @@ package com.example.internet_shop.productpromotions;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductPromotionRepository extends JpaRepository<ProductPromotion, ProductPromotionId> {
+
+    List<ProductPromotion> findById_ProductId(Long productId);
+
+    List<ProductPromotion> findById_PromotionId(Long promotionId);
+
+    void deleteById_ProductId(Long productId);
+
+    void deleteById_PromotionId(Long promotionId);
+
 }
