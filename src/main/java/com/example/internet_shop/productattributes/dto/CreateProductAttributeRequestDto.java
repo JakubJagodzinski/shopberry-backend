a44424df71d0e56b1dd4c8a87ad7472013d694e0
JagodaJakub <kubajagjag@gmail.com>
2025-05-12 01:24:15 +0200

    Added CRUD controller and all required classes including service, dtoMapper and relevant dtos for ProductAttribute entity

---
+++ src/main/java/com/example/internet_shop/productattributes/dto/CreateProductAttributeRequestDto.java
@@ -0,0 +1,20 @@
+package com.example.internet_shop.productattributes.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CreateProductAttributeRequestDto {
+
+    private Long productId;
+
+    private Long attributeId;
+
+    private Double value;
+
+}
