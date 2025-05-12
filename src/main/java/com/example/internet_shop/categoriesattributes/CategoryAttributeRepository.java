f3c125201d4465ea1e88ad47ee4b85bd077d45cf
JagodaJakub <kubajagjag@gmail.com>
2025-05-13 01:55:04 +0200

    Added CRUD controller and all required classes including service, dtoMapper and relevant dtos for CategoryAttribute entity

--- src/main/java/com/example/internet_shop/categoriesattributes/CategoryAttributeRepository.java
+++ src/main/java/com/example/internet_shop/categoriesattributes/CategoryAttributeRepository.java
@@ -3,6 +3,11 @@ package com.example.internet_shop.categoriesattributes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CategoryAttributeRepository extends JpaRepository<CategoryAttribute, CategoryAttributeId> {
+
+    List<CategoryAttribute> findByCategory_CategoryId(Long categoryId);
+
 }
---
+++ src/main/java/com/example/internet_shop/categoriesattributes/dto/CategoryAttributeResponseDto.java
@@ -0,0 +1,18 @@
+package com.example.internet_shop.categoriesattributes.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CategoryAttributeResponseDto {
+
+    private Long categoryId;
+
+    private Long attributeId;
+
+}
