3da32e02b876046180edd80dc07785c312ad9b66
JagodaJakub <kubajagjag@gmail.com>
2025-05-22 17:44:52 +0200

    Added new roles and relevant permissions

--- src/main/java/com/example/shopberry/user/Permission.java
+++ src/main/java/com/example/shopberry/user/Permission.java
@@ -7,17 +7,25 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum Permission {
 
+    GUEST_READ("guest:read"),
+    GUEST_UPDATE("guest:update"),
+    GUEST_CREATE("guest:create"),
+    GUEST_DELETE("guest:delete"),
+
+    CUSTOMER_READ("customer:read"),
+    CUSTOMER_UPDATE("customer:update"),
+    CUSTOMER_CREATE("customer:create"),
+    CUSTOMER_DELETE("customer:delete"),
+
+    EMPLOYEE_READ("employee:read"),
+    EMPLOYEE_UPDATE("employee:update"),
+    EMPLOYEE_CREATE("employee:create"),
+    EMPLOYEE_DELETE("employee:delete"),
+
     ADMIN_READ("admin:read"),
     ADMIN_UPDATE("admin:update"),
     ADMIN_CREATE("admin:create"),
-    ADMIN_DELETE("admin:delete"),
-
-    MANAGER_READ("management:read"),
-    MANAGER_UPDATE("management:update"),
-    MANAGER_CREATE("management:create"),
-    MANAGER_DELETE("management:delete");
-
-
+    ADMIN_DELETE("admin:delete");
 
     private final String permission;
 
--- src/main/java/com/example/shopberry/user/Role.java
+++ src/main/java/com/example/shopberry/user/Role.java
@@ -4,7 +4,6 @@ import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -15,27 +14,39 @@ import static com.example.shopberry.user.Permission.*;
 @RequiredArgsConstructor
 public enum Role {
 
-    USER(Collections.emptySet()),
+    GUEST(
+            Set.of(
+                    GUEST_READ,
+                    GUEST_UPDATE,
+                    GUEST_DELETE,
+                    GUEST_CREATE
+            )
+    ),
+
+    CUSTOMER(
+            Set.of(
+                    CUSTOMER_READ,
+                    CUSTOMER_UPDATE,
+                    CUSTOMER_DELETE,
+                    CUSTOMER_CREATE
+            )
+    ),
+
+    EMPLOYEE(
+            Set.of(
+                    EMPLOYEE_READ,
+                    EMPLOYEE_UPDATE,
+                    EMPLOYEE_DELETE,
+                    EMPLOYEE_CREATE
+            )
+    ),
 
     ADMIN(
             Set.of(
                     ADMIN_READ,
                     ADMIN_UPDATE,
                     ADMIN_DELETE,
-                    ADMIN_CREATE,
-                    MANAGER_READ,
-                    MANAGER_UPDATE,
-                    MANAGER_DELETE,
-                    MANAGER_CREATE
-            )
-    ),
-
-    MANAGER(
-            Set.of(
-                    MANAGER_READ,
-                    MANAGER_UPDATE,
-                    MANAGER_DELETE,
-                    MANAGER_CREATE
+                    ADMIN_CREATE
             )
     );
 
@@ -52,4 +63,4 @@ public enum Role {
         return authorities;
     }
 
-}
+    }
