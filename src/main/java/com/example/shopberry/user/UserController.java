ede60ce1821cebf21fd96aadb1e0e697507a283a
JagodaJakub <kubajagjag@gmail.com>
2025-05-22 15:50:08 +0200

    Created user package with permissions and roles

---
+++ src/main/java/com/example/shopberry/user/UserController.java
@@ -0,0 +1,30 @@
+package com.example.shopberry.user;
+
+import com.example.shopberry.user.dto.ChangePasswordRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserService userService;
+
+    @PatchMapping
+    public ResponseEntity<?> changePassword(@RequestBody ChangePasswordRequestDto changePasswordRequestDto, Principal connectedUser) {
+        userService.changePassword(changePasswordRequestDto, connectedUser);
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Password changed successfully");
+    }
+
+}
---
+++ src/main/java/com/example/shopberry/user/UserService.java
@@ -0,0 +1,40 @@
+package com.example.shopberry.user;
+
+import com.example.shopberry.user.dto.ChangePasswordRequestDto;
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import java.security.Principal;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    private final PasswordEncoder passwordEncoder;
+
+    private final String WRONG_PASSWORD_MESSAGE = "Wrong password";
+    private final String PASSWORDS_ARE_NOT_THE_SAME_MESSAGE = "Passwords are not the same";
+
+    @Transactional
+    public void changePassword(ChangePasswordRequestDto changePasswordRequestDto, Principal connectedUser) throws IllegalArgumentException {
+        User user = (User) ((UsernamePasswordAuthenticationToken) connectedUser).getPrincipal();
+
+        if (!passwordEncoder.matches(changePasswordRequestDto.getCurrentPassword(), user.getPassword())) {
+            throw new IllegalArgumentException(WRONG_PASSWORD_MESSAGE);
+        }
+
+        if (!changePasswordRequestDto.getNewPassword().equals(changePasswordRequestDto.getConfirmationPassword())) {
+            throw new IllegalArgumentException(PASSWORDS_ARE_NOT_THE_SAME_MESSAGE);
+        }
+
+        user.setPassword(passwordEncoder.encode(changePasswordRequestDto.getNewPassword()));
+
+        userRepository.save(user);
+    }
+
+}
