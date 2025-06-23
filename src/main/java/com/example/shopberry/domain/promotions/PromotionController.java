83d23c58d2e31bf94bac8c3646b70d30c9a8d5f2
JagodaJakub <kubajagjag@gmail.com>
2025-06-23 17:38:05 +0200

    Add OpenAPI documentation for promotion management endpoints

--- src/main/java/com/example/shopberry/domain/promotions/PromotionController.java
+++ src/main/java/com/example/shopberry/domain/promotions/PromotionController.java
@@ -4,7 +4,13 @@ import com.example.shopberry.auth.access.CheckPermission;
 import com.example.shopberry.domain.promotions.dto.request.CreatePromotionRequestDto;
 import com.example.shopberry.domain.promotions.dto.request.UpdatePromotionRequestDto;
 import com.example.shopberry.domain.promotions.dto.response.PromotionResponseDto;
+import com.example.shopberry.exception.ApiError;
 import com.example.shopberry.user.Permission;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,6 +29,17 @@ public class PromotionController {
 
     private final PromotionService promotionService;
 
+    @Operation(summary = "Get all promotions")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "List of promotions",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = PromotionResponseDto.class)
+                    )
+            )
+    })
     @GetMapping("/promotions")
     public ResponseEntity<List<PromotionResponseDto>> getAllPromotions() {
         List<PromotionResponseDto> promotionResponseDtoList = promotionService.getAllPromotions();
@@ -32,6 +49,25 @@ public class PromotionController {
                 .body(promotionResponseDtoList);
     }
 
+    @Operation(summary = "Get promotion by id")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Promotion found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = PromotionResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Promotion not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+    })
     @GetMapping("/promotions/{promotionId}")
     public ResponseEntity<PromotionResponseDto> getPromotionById(@PathVariable Long promotionId) {
         PromotionResponseDto promotionResponseDto = promotionService.getPromotionById(promotionId);
@@ -41,6 +77,33 @@ public class PromotionController {
                 .body(promotionResponseDto);
     }
 
+    @Operation(summary = "Create new promotions")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "New promotion created",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = PromotionResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Promotion with that name already exists",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+    })
     @CheckPermission(Permission.PROMOTION_CREATE)
     @PostMapping("/promotions")
     public ResponseEntity<PromotionResponseDto> createPromotion(@Valid @RequestBody CreatePromotionRequestDto createPromotionRequestDto) {
@@ -52,6 +115,41 @@ public class PromotionController {
                 .body(createdPromotionResponseDto);
     }
 
+    @Operation(summary = "Update promotion by id")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Promotion updated successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = PromotionResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Promotion with that name already exists",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Promotion not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+    })
     @CheckPermission(Permission.PROMOTION_UPDATE)
     @PatchMapping("/promotions/{promotionId}")
     public ResponseEntity<PromotionResponseDto> updatePromotionById(@PathVariable Long promotionId, @Valid @RequestBody UpdatePromotionRequestDto updatePromotionRequestDto) {
@@ -62,6 +160,29 @@ public class PromotionController {
                 .body(updatedPromotionResponseDto);
     }
 
+    @Operation(summary = "Delete promotion by id")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Promotion deleted successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Promotion not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+    })
     @CheckPermission(Permission.PROMOTION_DELETE)
     @DeleteMapping("/promotions/{promotionId}")
     public ResponseEntity<Void> deletePromotionById(@PathVariable Long promotionId) {
