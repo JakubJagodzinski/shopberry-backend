a2abdecb8fa75e56583546f226cd6ca802918ca3
JagodaJakub <kubajagjag@gmail.com>
2025-06-23 16:15:07 +0200

    Add OpenAPI documentation for order status management endpoints

--- src/main/java/com/example/shopberry/domain/orderstatuses/OrderStatusController.java
+++ src/main/java/com/example/shopberry/domain/orderstatuses/OrderStatusController.java
@@ -4,7 +4,13 @@ import com.example.shopberry.auth.access.CheckPermission;
 import com.example.shopberry.domain.orderstatuses.dto.request.CreateOrderStatusRequestDto;
 import com.example.shopberry.domain.orderstatuses.dto.request.UpdateOrderStatusRequestDto;
 import com.example.shopberry.domain.orderstatuses.dto.response.OrderStatusResponseDto;
+import com.example.shopberry.exception.ApiError;
 import com.example.shopberry.user.Permission;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,6 +29,17 @@ public class OrderStatusController {
 
     private final OrderStatusService orderStatusService;
 
+    @Operation(summary = "Get all order statuses")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "List of order statuses",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = OrderStatusResponseDto.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.ORDER_STATUS_READ_ALL)
     @GetMapping("/order-statuses")
     public ResponseEntity<List<OrderStatusResponseDto>> getAllOrderStatuses() {
@@ -33,6 +50,25 @@ public class OrderStatusController {
                 .body(orderStatusResponseDtoList);
     }
 
+    @Operation(summary = "Get order status by id")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Order status found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = OrderStatusResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Order status not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.ORDER_STATUS_READ)
     @GetMapping("/order-statuses/{orderStatusId}")
     public ResponseEntity<OrderStatusResponseDto> getOrderStatusById(@PathVariable Long orderStatusId) {
@@ -43,6 +79,33 @@ public class OrderStatusController {
                 .body(orderStatusResponseDto);
     }
 
+    @Operation(summary = "Create a new order status")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Order status created",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = OrderStatusResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Order status with that name already exists",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.ORDER_STATUS_CREATE)
     @PostMapping("/order-statuses")
     public ResponseEntity<OrderStatusResponseDto> createOrderStatus(@Valid @RequestBody CreateOrderStatusRequestDto createOrderStatusRequestDto) {
@@ -54,6 +117,41 @@ public class OrderStatusController {
                 .body(createdOrderStatusResponseDto);
     }
 
+    @Operation(summary = "Update order status by ID")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Order status updated",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = OrderStatusResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Order status with than name already exists",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Order status not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.ORDER_STATUS_UPDATE)
     @PatchMapping("/order-statuses/{orderStatusId}")
     public ResponseEntity<OrderStatusResponseDto> updateOrderStatusById(@PathVariable Long orderStatusId, @Valid @RequestBody UpdateOrderStatusRequestDto updateOrderStatusRequestDto) {
@@ -64,6 +162,29 @@ public class OrderStatusController {
                 .body(updatedOrderStatusResponseDto);
     }
 
+    @Operation(summary = "Delete order status by ID")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Order status deleted"
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Order status not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.ORDER_STATUS_DELETE)
     @DeleteMapping("/order-statuses/{orderStatusId}")
     public ResponseEntity<Void> deleteOrderStatusById(@PathVariable Long orderStatusId) {
