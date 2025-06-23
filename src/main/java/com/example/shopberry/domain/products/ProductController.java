56fe860c0ebaa651864974cbde2b5d99728760f6
JagodaJakub <kubajagjag@gmail.com>
2025-06-23 17:31:57 +0200

    Add OpenAPI documentation for product management endpoints

--- src/main/java/com/example/shopberry/domain/products/ProductController.java
+++ src/main/java/com/example/shopberry/domain/products/ProductController.java
@@ -5,7 +5,13 @@ import com.example.shopberry.domain.productattributes.dto.response.ProductWithAt
 import com.example.shopberry.domain.products.dto.request.CreateProductRequestDto;
 import com.example.shopberry.domain.products.dto.request.UpdateProductRequestDto;
 import com.example.shopberry.domain.products.dto.response.ProductResponseDto;
+import com.example.shopberry.exception.ApiError;
 import com.example.shopberry.user.Permission;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -24,6 +30,25 @@ public class ProductController {
 
     private final ProductService productService;
 
+    @Operation(summary = "Get all products with its attributes by category id or product name matching")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "List of products",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ProductWithAttributesResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Category not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @GetMapping("/products")
     public ResponseEntity<List<ProductWithAttributesResponseDto>> getAllProductsWithParams(@RequestParam(required = false) Long categoryId, @RequestParam(required = false) String productName) {
         List<ProductWithAttributesResponseDto> productResponseDtoList = productService.getAllProductsWithParams(categoryId, productName);
@@ -33,6 +58,25 @@ public class ProductController {
                 .body(productResponseDtoList);
     }
 
+    @Operation(summary = "Get product by id")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Product found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ProductResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Product not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @GetMapping("/products/{productId}")
     public ResponseEntity<ProductResponseDto> getProductById(@PathVariable Long productId) {
         ProductResponseDto productResponseDto = productService.getProductById(productId);
@@ -42,6 +86,41 @@ public class ProductController {
                 .body(productResponseDto);
     }
 
+    @Operation(summary = "Create new product")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "New product created",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ProductResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Cannot add product to parent category",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Producer / category not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.PRODUCT_CREATE)
     @PostMapping("/products")
     public ResponseEntity<ProductResponseDto> createProduct(@Valid @RequestBody CreateProductRequestDto createProductRequestDto) {
@@ -53,6 +132,41 @@ public class ProductController {
                 .body(createdProductResponseDto);
     }
 
+    @Operation(summary = "Update product by id")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Product updated successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ProductResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Cannot set parent category as product category",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Product / producer / category not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.PRODUCT_UPDATE)
     @PatchMapping("/products/{productId}")
     public ResponseEntity<ProductResponseDto> updateProductById(@PathVariable Long productId, @Valid @RequestBody UpdateProductRequestDto updateProductRequestDto) {
@@ -63,6 +177,29 @@ public class ProductController {
                 .body(updatedProductResponseDto);
     }
 
+    @Operation(summary = "Delete product by id")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Product deleted successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Product not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.PRODUCT_DELETE)
     @DeleteMapping("/products/{productId}")
     public ResponseEntity<Void> deleteProductById(@PathVariable Long productId) {
