51b8467788204c6dcd11f2269091e19a1209c706
JagodaJakub <kubajagjag@gmail.com>
2025-06-23 17:43:55 +0200

    Add OpenAPI documentation for product return management endpoints

--- src/main/java/com/example/shopberry/domain/productreturns/ProductReturnController.java
+++ src/main/java/com/example/shopberry/domain/productreturns/ProductReturnController.java
@@ -3,7 +3,13 @@ package com.example.shopberry.domain.productreturns;
 import com.example.shopberry.auth.access.CheckPermission;
 import com.example.shopberry.domain.productreturns.dto.request.CreateProductReturnRequestDto;
 import com.example.shopberry.domain.productreturns.dto.response.ProductReturnResponseDto;
+import com.example.shopberry.exception.ApiError;
 import com.example.shopberry.user.Permission;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -22,6 +28,33 @@ public class ProductReturnController {
 
     private final ProductReturnService productReturnService;
 
+    @Operation(summary = "Get product return by id")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Product return found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ProductReturnResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Promotion return not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+    })
     @CheckPermission(Permission.PRODUCT_RETURN_READ)
     @GetMapping("/product-returns/{productReturnId}")
     public ResponseEntity<ProductReturnResponseDto> getProductReturnById(@PathVariable Long productReturnId) {
@@ -32,6 +65,33 @@ public class ProductReturnController {
                 .body(productReturnResponseDto);
     }
 
+    @Operation(summary = "Get order all product returns")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "List of order all product returns",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ProductReturnResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Order not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+    })
     @CheckPermission(Permission.ORDER_PRODUCT_RETURN_READ_ALL)
     @GetMapping("/orders/{orderId}/product-returns")
     public ResponseEntity<List<ProductReturnResponseDto>> getOrderAllProductReturns(@PathVariable Long orderId) {
@@ -42,6 +102,41 @@ public class ProductReturnController {
                 .body(productReturnResponseDtoList);
     }
 
+    @Operation(summary = "Create product return for order")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Product return created successfully",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ProductReturnResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Product return already exists / product does not belong to that order",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Order / product not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+    })
     @CheckPermission(Permission.PRODUCT_RETURN_CREATE)
     @PostMapping("/orders/{orderId}/product-returns")
     public ResponseEntity<ProductReturnResponseDto> createProductReturn(@PathVariable Long orderId, @Valid @RequestBody CreateProductReturnRequestDto createProductReturnRequestDto) {
@@ -53,6 +148,29 @@ public class ProductReturnController {
                 .body(createdProductReturnResponseDto);
     }
 
+    @Operation(summary = "Delete product return by id")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Product return deleted successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Product return not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+    })
     @CheckPermission(Permission.PRODUCT_RETURN_DELETE)
     @DeleteMapping("/product-returns/{productReturnId}")
     public ResponseEntity<Void> deleteProductReturnById(@PathVariable Long productReturnId) {
