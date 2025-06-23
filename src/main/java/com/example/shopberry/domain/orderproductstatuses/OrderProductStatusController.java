84b8be0d57f00095a5aa073e4a588049674f8d66
JagodaJakub <kubajagjag@gmail.com>
2025-06-23 16:06:38 +0200

    Add OpenAPI documentation for order product status endpoints

--- src/main/java/com/example/shopberry/domain/orderproductstatuses/OrderProductStatusController.java
+++ src/main/java/com/example/shopberry/domain/orderproductstatuses/OrderProductStatusController.java
@@ -4,7 +4,13 @@ import com.example.shopberry.auth.access.CheckPermission;
 import com.example.shopberry.domain.orderproductstatuses.dto.request.CreateOrderProductStatusRequestDto;
 import com.example.shopberry.domain.orderproductstatuses.dto.request.UpdateOrderProductStatusRequestDto;
 import com.example.shopberry.domain.orderproductstatuses.dto.response.OrderProductStatusResponseDto;
+import com.example.shopberry.exception.ApiError;
 import com.example.shopberry.user.Permission;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -23,6 +29,17 @@ public class OrderProductStatusController {
 
     private final OrderProductStatusService orderProductStatusService;
 
+    @Operation(summary = "Get all order product statuses")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "List of order product statuses",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = OrderProductStatusResponseDto.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.ORDER_PRODUCT_STATUS_READ_ALL)
     @GetMapping("/order-product-statuses")
     public ResponseEntity<List<OrderProductStatusResponseDto>> getAllOrderProductStatuses() {
@@ -33,6 +50,25 @@ public class OrderProductStatusController {
                 .body(orderProductStatusResponseDtoList);
     }
 
+    @Operation(summary = "Get order product status by id")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Order product status found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = OrderProductStatusResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Order product status not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.ORDER_PRODUCT_STATUS_READ)
     @GetMapping("/order-product-statuses/{orderProductStatusId}")
     public ResponseEntity<OrderProductStatusResponseDto> getOrderProductStatusById(@PathVariable Long orderProductStatusId) {
@@ -43,6 +79,33 @@ public class OrderProductStatusController {
                 .body(orderProductStatusResponseDto);
     }
 
+    @Operation(summary = "Create a new order product status")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Order product status created",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = OrderProductStatusResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Order product status with that name already exists",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.ORDER_PRODUCT_STATUS_UPDATE)
     @PostMapping("/order-product-statuses")
     public ResponseEntity<OrderProductStatusResponseDto> createOrderProductStatus(@Valid @RequestBody CreateOrderProductStatusRequestDto createOrderProductStatusRequestDto) {
@@ -54,6 +117,41 @@ public class OrderProductStatusController {
                 .body(createdOrderProductStatusResponseDto);
     }
 
+    @Operation(summary = "Update order product status by ID")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Order product status updated",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = OrderProductStatusResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Order product status with than name already exists",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Order product status not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.ORDER_PRODUCT_STATUS_UPDATE)
     @PatchMapping("/order-product-statuses/{orderProductStatusId}")
     public ResponseEntity<OrderProductStatusResponseDto> updateOrderProductStatusById(@PathVariable Long orderProductStatusId, @Valid @RequestBody UpdateOrderProductStatusRequestDto updateOrderProductStatusRequestDto) {
@@ -64,6 +162,29 @@ public class OrderProductStatusController {
                 .body(updatedOrderProductStatusResponseDto);
     }
 
+    @Operation(summary = "Delete order product status by ID")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Order product status deleted"
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Order product status not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.ORDER_PRODUCT_STATUS_DELETE)
     @DeleteMapping("/order-product-statuses/{orderProductStatusId}")
     public ResponseEntity<Void> deleteOrderProductStatusById(@PathVariable Long orderProductStatusId) {
