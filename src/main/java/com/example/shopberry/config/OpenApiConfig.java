87832d7909d89bb01bbc78822d2c70b1d9c91aae
JagodaJakub <kubajagjag@gmail.com>
2025-06-23 14:21:48 +0200

    Remove unused global responses customizer and related imports in OpenApiConfig

--- src/main/java/com/example/shopberry/config/OpenApiConfig.java
+++ src/main/java/com/example/shopberry/config/OpenApiConfig.java
@@ -1,16 +1,12 @@
 package com.example.shopberry.config;
 
 import com.example.shopberry.common.constants.ApiConstants;
-import com.example.shopberry.common.constants.HttpStatusConstants;
 import com.example.shopberry.common.constants.SecurityConstants;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.responses.ApiResponse;
-import io.swagger.v3.oas.models.responses.ApiResponses;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import org.springdoc.core.customizers.OpenApiCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -38,36 +34,6 @@ public class OpenApiConfig {
                                                 .bearerFormat(SecurityConstants.BEARER_FORMAT_JWT)
                                 )
                 );
-    }
-
-    @Bean
-    public OpenApiCustomizer globalResponsesCustomizer() {
-        return openApi -> openApi.getPaths().forEach((path, pathItem) ->
-                pathItem.readOperations().forEach(operation -> {
-                    ApiResponses responses = operation.getResponses();
-
-                    if (!responses.containsKey(HttpStatusConstants.HTTP_STATUS_BAD_REQUEST)) {
-                        responses.addApiResponse(
-                                HttpStatusConstants.HTTP_STATUS_BAD_REQUEST,
-                                new ApiResponse().description(HttpStatusConstants.HTTP_DESCRIPTION_BAD_REQUEST)
-                        );
-                    }
-
-                    if (!responses.containsKey(HttpStatusConstants.HTTP_STATUS_FORBIDDEN)) {
-                        responses.addApiResponse(
-                                HttpStatusConstants.HTTP_STATUS_FORBIDDEN,
-                                new ApiResponse().description(HttpStatusConstants.HTTP_DESCRIPTION_FORBIDDEN)
-                        );
-                    }
-
-                    if (!responses.containsKey(HttpStatusConstants.HTTP_STATUS_NOT_FOUND)) {
-                        responses.addApiResponse(
-                                HttpStatusConstants.HTTP_STATUS_NOT_FOUND,
-                                new ApiResponse().description(HttpStatusConstants.HTTP_DESCRIPTION_NOT_FOUND)
-                        );
-                    }
-                })
-        );
     }
 
 }
