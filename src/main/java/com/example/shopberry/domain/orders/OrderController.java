2e2120b33f2202c79db4e143e36e6403d10f6de5
JagodaJakub <kubajagjag@gmail.com>
2025-06-23 16:56:40 +0200

    Add OpenAPI documentation for order management endpoints

--- src/main/java/com/example/shopberry/domain/orders/OrderController.java
+++ src/main/java/com/example/shopberry/domain/orders/OrderController.java
@@ -4,7 +4,13 @@ import com.example.shopberry.auth.access.CheckPermission;
 import com.example.shopberry.domain.orders.dto.request.CreateOrderRequestDto;
 import com.example.shopberry.domain.orders.dto.request.UpdateOrderRequestDto;
 import com.example.shopberry.domain.orders.dto.response.OrderResponseDto;
+import com.example.shopberry.exception.ApiError;
 import com.example.shopberry.user.Permission;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -24,6 +30,25 @@ public class OrderController {
 
     private final OrderService orderService;
 
+    @Operation(summary = "Get all orders")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "List of orders",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = OrderResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.ORDER_READ_ALL)
     @GetMapping("/orders")
     public ResponseEntity<List<OrderResponseDto>> getAllOrders() {
@@ -34,6 +59,33 @@ public class OrderController {
                 .body(orderResponseDtoList);
     }
 
+    @Operation(summary = "Get order by id")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Order found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = OrderResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Order not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.ORDER_READ)
     @GetMapping("/orders/{orderId}")
     public ResponseEntity<OrderResponseDto> getOrderById(@PathVariable Long orderId) {
@@ -44,6 +96,33 @@ public class OrderController {
                 .body(orderResponseDto);
     }
 
+    @Operation(summary = "Get customer all orders")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "List of customer's orders",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = OrderResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Customer not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.CUSTOMER_ORDER_READ_ALL)
     @GetMapping("/customers/{customerId}/orders")
     public ResponseEntity<List<OrderResponseDto>> getCustomerAllOrders(@PathVariable UUID customerId) {
@@ -54,6 +133,33 @@ public class OrderController {
                 .body(orderResponseDtoList);
     }
 
+    @Operation(summary = "Create order for customer")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "201",
+                    description = "Created order",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = OrderResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Customer not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.ORDER_CREATE)
     @PostMapping("/orders")
     public ResponseEntity<OrderResponseDto> createOrder(@Valid @RequestBody CreateOrderRequestDto createOrderRequestDto) {
@@ -65,6 +171,33 @@ public class OrderController {
                 .body(createdOrderResponseDto);
     }
 
+    @Operation(summary = "Update order by id")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Updated order",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = OrderResponseDto.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Order not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.ORDER_UPDATE)
     @PatchMapping("/orders/{orderId}")
     public ResponseEntity<OrderResponseDto> updateOrderById(@PathVariable Long orderId, @Valid @RequestBody UpdateOrderRequestDto updateOrderRequestDto) {
@@ -75,6 +208,29 @@ public class OrderController {
                 .body(updatedOrderResponseDto);
     }
 
+    @Operation(summary = "Delete order by id")
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "204",
+                    description = "Order deleted successfully"
+            ),
+            @ApiResponse(
+                    responseCode = "403",
+                    description = "Access Denied",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "Order / shipment type / payment type not found",
+                    content = @Content(
+                            mediaType = "application/json",
+                            schema = @Schema(implementation = ApiError.class)
+                    )
+            )
+    })
     @CheckPermission(Permission.ORDER_DELETE)
     @DeleteMapping("/orders/{orderId}")
     public ResponseEntity<Void> deleteOrderById(@PathVariable Long orderId) {
